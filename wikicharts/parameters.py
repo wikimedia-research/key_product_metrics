853aa74777700fdc8a347938d5ff3e9504b1b929
Neil Shah-Quinn <nshahquinn@wikimedia.org>
2023-09-30 11:28:03 +0000

    Blank repo after merge

--- wikicharts/parameters.py
+++
@@ -1,5 +0,0 @@
-editing_data_path = "../../Editing-movement-metrics/metrics/metrics.tsv"
-readers_data_path = "../../Readers-movement-metrics/metrics/metrics.tsv"
-unique_devices_data_path = "../../Readers-movement-metrics/metrics/metrics-unique-devices.tsv"
-save_directory = "../../Readers-movement-metrics/charts/"
-author = "Maya Kampurath"
